9079b6587120fd82a1df21e86f5db6353e313dd8
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2024-03-25 17:53:25 +0530

    fdasdf;

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,7 +6,21 @@ app = Flask(__name__)
 def home():
     return 'Hello, world!'
 
-@app.route("/mbsa")
+@app.route('/mbsa')
 def mbsa():
     return render_template('index.html')
 
+@app.route('/api/post_example', methods=['POST'])
+def post_example():
+    if request.method == 'POST':
+        # Get the user input from the request
+        user_input = request.form.get('user_input')
+        
+        # Return the user input as a JSON response
+        return jsonify({'user_input': user_input}), 200
+    else:
+        # Return an error if the request method is not POST
+        return jsonify({'error': 'Method not allowed'}), 405
+
+if __name__ == '__main__':
+    app.run(debug=True)
