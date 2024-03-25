6a9214f792f743f3e4110d8dae72288fea63b2f4
Sanket Dhuri <contact.sanketdhuri@gmail.com>
2024-03-25 18:17:45 +0530

    ojsf

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, jsonify
-import requests
-from uuid import uuid4
-from dotenv import load_dotenv
-import os
+# import requests
+# from uuid import uuid4
+# from dotenv import load_dotenv
+# import os
 
 app = Flask(__name__)
 
